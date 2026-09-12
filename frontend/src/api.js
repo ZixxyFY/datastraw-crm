0de6039a35d7055a7dbcc494fd47f32b8ffed3b8
ZixxyFY <neeraj112005sahu@gmail.com>
2026-09-12 20:14:46 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://datastraw-crm-backend-n9ca.onrender.com/api',
 });
 
 export default API;
